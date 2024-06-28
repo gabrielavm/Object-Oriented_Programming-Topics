cb6a0363a742aec2065b36db8411bc4d367a4f7a
Gabriela Mladenova <115610325+gabrielavm@users.noreply.github.com>
2024-06-28 21:46:34 +0300

    Create ConsolePrinter.cpp

---
+++ Topic 5/Aggregation example/ConsolePrinter.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "ConsolePrinter.h"
+
+ConsolePrinter::ConsolePrinter() : shouldCensore([](char ch) {return false; })
+{}
+
+ConsolePrinter::ConsolePrinter(bool (*pred)(char ch)) : shouldCensore(pred)
+{}
+
+void ConsolePrinter::print(const char* str) const
+{
+	while (*str)
+	{
+		if (shouldCensore(*str))
+		{
+			std::cout << "*";
+		}
+		else
+		{
+			std::cout << *str;
+		}
+		str++;
+	}
+	std::cout << std::endl;
+}
