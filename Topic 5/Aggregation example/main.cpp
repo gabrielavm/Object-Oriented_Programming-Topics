0ab4ebb94ea8e5713e56881523c048bef59c8313
Gabriela Mladenova <115610325+gabrielavm@users.noreply.github.com>
2024-06-28 21:54:57 +0300

    Create main.cpp

---
+++ Topic 5/Aggregation example/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "ConsolePrinter.h"
+#include "NumberWrapper.h"
+#include "StringWrapper.h"
+
+int main()
+{
+	{
+		ConsolePrinter noNumbers([](char ch) {return ch >= '0' && ch <= '9'; });
+
+		StringWrapper sr("Test3432wrwe", noNumbers);
+
+		NumberWrapper nw(12345, noNumbers);
+
+		sr.print();
+		nw.print();
+	}
+	{
+		ConsolePrinter censoreEverything([](char ch) {return true; });
+
+		StringWrapper sr("Test3432wrwe", censoreEverything);
+
+		NumberWrapper nw(12345, censoreEverything);
+
+		sr.print();
+		nw.print();
+	}
+
+}
