10b22b353ec4540704acaf05ad0c05c92f3ea504
Gabriela Mladenova <115610325+gabrielavm@users.noreply.github.com>
2024-06-28 21:47:40 +0300

    Create NumberWrapper.h

---
+++ Topic 5/Aggregation example/NumberWrapper.h
@@ -0,0 +1,16 @@
+#include "ConsolePrinter.h"
+
+class NumberWrapper
+{
+private:
+
+	int n;
+	const ConsolePrinter& consolePrinter;
+
+public:
+
+	NumberWrapper(int n, const ConsolePrinter& cp);
+
+	int getNumber() const;
+	void print() const;
+};
