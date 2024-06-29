370484ec84861a43f36e7f69545386baacc76e62
Gabriela Mladenova <115610325+gabrielavm@users.noreply.github.com>
2024-06-29 20:30:08 +0300

    Create Student.h

---
+++ Topic 10/Person,Student,Teacher/Student.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+#include "Person.h"
+
+class Student : public Person
+{
+	unsigned facultyNum = 0;
+
+public:
+
+	Student();
+	Student(const char* name, unsigned age, unsigned facultyNum);
+
+	void setFacultyNum(unsigned facultyNum);
+	unsigned getFacultyNum() const;
+};
