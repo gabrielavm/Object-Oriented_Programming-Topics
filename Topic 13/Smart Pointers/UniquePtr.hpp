3cdb4e5fa2cec0918f7e5c98add9b9f4fc48965b
Gabriela Mladenova <115610325+gabrielavm@users.noreply.github.com>
2024-06-30 20:05:26 +0300

    Create UniquePtr.hpp

---
+++ Topic 13/Smart Pointers/UniquePtr.hpp
@@ -0,0 +1,80 @@
+#pragma once
+#include <iostream>
+
+template<typename T>
+class UniquePtr
+{
+	T* data;
+
+public:
+
+	UniquePtr(T* ptr);
+
+	UniquePtr(const UniquePtr& other) = delete;
+	UniquePtr(UniquePtr&& other) noexcept;
+
+	UniquePtr& operator=(const UniquePtr& other) = delete;
+	UniquePtr& operator=(UniquePtr&& other) noexcept;
+
+	~UniquePtr();
+
+	T& operator*();
+	const T& operator*() const;
+
+	T& operator->();
+	const T& operator->() const;
+};
+
+template<typename T>
+UniquePtr<T>::UniquePtr(T* ptr) : data(ptr)
+{}
+
+template<typename T>
+UniquePtr<T>::UniquePtr(UniquePtr&& other) noexcept
+{
+	data = other.data;
+	other.data = nullptr;
+}
+
+template<typename T>
+UniquePtr<T>& UniquePtr<T>::operator=(UniquePtr&& other) noexcept
+{
+	if (this != &other)
+	{
+		delete data;
+		data = other.data;
+		other.data = nullptr;
+	}
+
+	return *this;
+}
+
+template<typename T>
+UniquePtr<T>::~UniquePtr()
+{
+	delete data;
+}
+
+template<typename T>
+T& UniquePtr<T>::operator*()
+{
+	return *data;
+}
+
+template<typename T>
+const T& UniquePtr<T>::operator*() const
+{
+	return *data;
+}
+
+template<typename T>
+T& UniquePtr<T>::operator->()
+{
+	return *data;
+}
+
+template<typename T>
+const T& UniquePtr<T>::operator->() const
+{
+	return *data;
+}
