aa26549a0cfdad4bedb2bd2091f14b5df1f072f9
Gabriela Mladenova <115610325+gabrielavm@users.noreply.github.com>
2024-06-30 16:02:37 +0300

    Create Dog.cpp

---
+++ Topic 12/Polymorphic Container Example/Farm/Dog.cpp
@@ -0,0 +1,14 @@
+#include "Dog.h"
+
+Dog::Dog() : Animal(AnimalType::Dog)
+{}
+
+void Dog::roar() const
+{
+	std::cout << "Wof wof" << std::endl;
+}
+
+Animal* Dog::clone() const
+{
+	return new Dog(*this);
+}
