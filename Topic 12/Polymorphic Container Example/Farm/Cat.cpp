52716b4373a1bd455723a12a15c31b12e32bdc08
Gabriela Mladenova <115610325+gabrielavm@users.noreply.github.com>
2024-06-30 16:04:59 +0300

    Create Cat.cpp

---
+++ Topic 12/Polymorphic Container Example/Farm/Cat.cpp
@@ -0,0 +1,14 @@
+#include "Cat.h"
+
+Cat::Cat() : Animal(AnimalType::Cat)
+{}
+
+void Cat::roar() const
+{
+	std::cout << "Meow meow" << std::endl;
+}
+
+Animal* Cat::clone() const
+{
+	return new Cat(*this);
+}
